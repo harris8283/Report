7b1ca4f5ed92362b4513ad854318e54a3b6753f9
ShiHaoXiang <114844744+ShiHaoXiang@users.noreply.github.com>
2023-10-27 22:47:21 +0800

    更新

--- js/LogIn.js
+++ js/LogIn.js
@@ -7,17 +7,19 @@ var app = new Vue({
     methods: {
         LogIn: function() {
             var Url = "http://www.HaoShiang.somee.com/Public/LogIn.ashx?UserName=" + this.UserName + "&PassWord=" + this.PassWord;
-            $.ajax({
-                url: Url,
-                type: 'GET',
-                dataType: 'json', // 預期的回應數據類型
-                success: function(data) {
-                    alert("123")
+            this.$http.get(Url).then(
+                function(response){
+                    var res = JSON.parse(response);
+                    alert(res.Message);
+                    if(res.Code == "0"){
+                        window.location.href = Index.html;
+                    }
                 },
-                error: function(error) {
-                    alert("456")
+                function(error){
+                    alert("登入失敗")
+                    console.log(error)
                 }
-            });
+            )
         },
     },
 })
