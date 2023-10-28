86667bb821ae1a4c8d1eae8feabdd8aa91756789
ShiHaoXiang <114844744+ShiHaoXiang@users.noreply.github.com>
2023-10-28 10:50:27 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 var app = new Vue({
     el: '#app',
     data: {
-        
+        GUID: '',
+        ChinName: '',
     },
     mounted: function() {
-
+        this.init();
     },
     methods: {
+        init: function() {
+            if(sessionStorage.getItem("GUID") != ""){
+                this.GUID = sessionStorage.getItem("GUID");
+            }
+            if(sessionStorage.getItem("ChinName") != ""){
+                this.ChinName = sessionStorage.getItem("ChinName");
+            }
+            sessionStorage.clear();
+        },
         LogIn: function() {
             window.location.href = "LogIn.html";
         },
