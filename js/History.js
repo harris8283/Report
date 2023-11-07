11e5ae93203afcf05d06568f8f386cffd2bfccac
時浩翔\a0963 <a0963574522@gmail.com>
2023-11-07 22:41:37 +0800

    更新

--- js/History.js
+++ js/History.js
@@ -1,12 +1,29 @@
 var History = new Vue({
     el: '#History',
     data: {
-
+        StartDate: '',
+        EndDate: '',
     },
     mounted: function() {
-
+        this.init();
     },
     methods: {
-
+        init: function() {
+            this.GetDate();
+        },
+        GetDate: function() {
+            var date = new Date();
+            var Year = date.getFullYear();
+            var Month = date.getMonth() + 1;
+            if(Month.toString().length == 1){
+                Month = "0" + Month;
+            }
+            var Day = date.getDate();
+            if(Day.toString().length == 1){
+                Day = "0" + Day;
+            }
+            this.StartDate = Year + "-" + Month + "-01";
+            this.EndDate = Year + "-" + Month + "-" + Day;
+        },
     },
 })
